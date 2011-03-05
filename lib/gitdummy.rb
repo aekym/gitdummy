53020ed52d502cb07ec59b0d7f934f102015394b
aekym <me@aekym.com>
2011-03-05 03:16:34 +0100

    use minitest for all the check logic

--- lib/gitdummy.rb
+++ lib/gitdummy.rb
@@ -1,3 +1,19 @@
+require 'grit'
+require 'minitest/unit'
+
 module Gitdummy
-  # Your code goes here...
+
+  def self.start
+    MiniTest::Unit.autorun
+
+    $repo = Grit::Repo.new('.')
+
+    test_dir = File.realpath('../lib/gitdummy/tests', File.dirname(__FILE__))
+
+    Dir["#{test_dir}/**/*_test.rb"].each do |file|
+      require file
+    end
+  end
+
 end
+
