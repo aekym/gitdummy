6d49d308947714eb45af7d4994e8814913ec679d
aekym <me@aekym.com>
2011-03-06 04:22:32 +0100

    allow custom regexp to test user's name and email via .gitdummyrc

--- lib/gitdummy.rb
+++ lib/gitdummy.rb
@@ -1,15 +1,22 @@
+# -*- encoding: utf-8 -*-
+#
 require 'grit'
 require 'minitest/unit'
 require 'thor'
+require 'yaml'
 
 
 module Gitdummy
   class Cli < Thor
     default_task :check
 
+    CONFIG_FILE = '.gitdummyrc'
+
     def initialize(*args)
       super
+
       $repo = Grit::Repo.new('.')
+      $config = YAML.load_file(CONFIG_FILE) if File.readable?(CONFIG_FILE)
     end
 
     desc 'check', 'Check your current git repository and configuration'
--- lib/gitdummy/tests/config_test.rb
+++ lib/gitdummy/tests/config_test.rb
@@ -1,19 +1,27 @@
+# -*- encoding: utf-8 -*-
+
 class ConfigTest < MiniTest::Unit::TestCase
 
   def test_user_name
-    name = $repo.config['user.name']
+    name = $repo.config['user.name'].force_encoding('utf-8')
 
-    assert_match /^[A-Z]\w+ [A-Z]\w+$/, name,
-      "Please correct your user.name configuration with e.g. `git config user.name 'John Doe'`.
-`#{name}` not accepted."
+    # load configurable regexp to test the user's email
+    #
+    regexp = Regexp.new($config['tests']['config']['user']['name_regexp'].force_encoding('utf-8'))
+
+    assert_match regexp || /^\w+ \w+$/i, name,
+      "Please correct your user.name configuration with e.g. `git config user.name 'John Doe'`. `#{name}` not accepted"
   end
 
   def test_user_email
-    email = $repo.config['user.email']
+    email = $repo.config['user.email'].force_encoding('utf-8')
 
-    assert_match /^[a-z]+\.[a-z]+@\w+\.\w+$/, email,
-      "Please correct your user.email configuration with e.g. `git config user.email 'john.doe@example.com'`.
-`#{email}` not accepted."
+    # load configurable regexp to test the user's email
+    #
+    regexp = Regexp.new($config['tests']['config']['user']['email_regexp'].force_encoding('utf-8'))
+
+    assert_match regexp || /^[a-z]+\.[a-z]+@\w+\.\w+$/, email,
+      "Please correct your user.email configuration with e.g. `git config user.email 'john.doe@example.com'`. `#{email}` not accepted"
   end
 
   def test_default_tracking
